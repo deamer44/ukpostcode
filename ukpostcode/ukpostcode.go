9097ed367355f852af6c109d4fa587da76224af2
Thomas Deamer <tom.deamer@googlemail.com>
2024-03-24 10:51:05 +0000

    added somments

--- ukpostcode/ukpostcode.go
+++ ukpostcode/ukpostcode.go
@@ -38,6 +38,7 @@ func (p *PostcodeList) Search(postcode string) (Postcode, error) {
 }
 
 func CheckPostcode(postcode string) (string, error) {
+	//postcode needs to have a space e.g. GL52 2SN
 	postcode = strings.ToUpper(postcode)
 	p := regexp.MustCompile("^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([AZa-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) [0-9][A-Za-z]{2})$").MatchString(postcode)
 	if p {
